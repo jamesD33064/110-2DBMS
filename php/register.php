43ba0be7b18c2d6de077c61a3152cc1a3ebee145
D33064 <james0120160379@gmail.com>
2022-03-16 10:57:10 +0800

    .

--- php/register.php
+++ php/register.php
@@ -1,24 +1,25 @@
 <?php
-    $dbname="fcu";
+    $dbname="1102DBMS";
 
-    $link = mysqli_connect("localhost" , $dbname , "A123456789" , $dbname) 
+    $link = mysqli_connect("localhost" , $dbname , "123" , $dbname) 
             or die("fault");
     echo "successful<br>";
     
+    $id=$_POST["id"];
     $username=$_POST["username"];
     $email=$_POST["email"];
     $password=$_POST["password"];
 
-    $sql = " SELECT * FROM `student`; ";
+    $sql = " SELECT * FROM `顧客`; ";
 
-    $check="SELECT * FROM student WHERE id='".$username."'";
+    $check="SELECT * FROM 顧客 WHERE 身分證字號='".$id."'";
 
     if(mysqli_num_rows(mysqli_query($link,$check))==0){
-        $sql = "INSERT INTO student (id, password)
-                VALUES('".$username."','".$password."')";
+        $sql = "INSERT INTO 顧客 (身分證字號 , 暱稱 , 密碼)
+                VALUES('".$id."','".$username."','".$password."')";
         if(mysqli_query($link, $sql)){
             echo "註冊成功!3秒後將自動跳轉頁面<br>";
-            echo "<a href='index.php'>未成功跳轉頁面請點擊此</a>";
+            echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
             // header("refresh:32;url=index.php");
             exit;
         }
