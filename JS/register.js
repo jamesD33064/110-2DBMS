5d3ccc4566392436601e3a307cd6a6d5b71e7905
D33064 <james0120160379@gmail.com>
2022-03-16 11:13:45 +0800

    .

---
+++ JS/register.js
@@ -0,0 +1,11 @@
+function test(){
+
+var num_1 = document.getElementById("form3Example4cg").value;
+var num_2 = document.getElementById("form3Example4cdg").value;
+if(num_1 != num_2){
+    alert("密碼輸錯");
+    return false;
+}
+
+
+}
