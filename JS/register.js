22ad65bc4b705a6b080a6bc6242b25e0a453d40d
jamesD33064 <james0120160379@gmail.com>
2022-03-16 16:37:42 +0800

    .

--- JS/register.js
+++ JS/register.js
@@ -1,8 +1,8 @@
 function test(){
 
-    var num_1 = document.getElementById("form3Example4cg").value;
-    var num_2 = document.getElementById("form3Example4cdg").value;
-    var check = document.getElementById("form2Example3cg").value;
+    var password_1 = document.getElementById("form3Example4cg").value;
+    var password_2 = document.getElementById("form3Example4cdg").value;
+    var agree = document.getElementById("form2Example3cg").value;
     if(num_1 != num_2){
         alert("密碼輸入錯誤");
         return false;
