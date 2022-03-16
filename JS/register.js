7aeb179ad3c47b83997177c9e5daeca50b5d9768
D33064 <james0120160379@gmail.com>
2022-03-16 20:15:26 +0800

    .

--- JS/register.js
+++ JS/register.js
@@ -3,7 +3,7 @@ function test(){
     var id = document.getElementById("form3Example1cg").value;
     var password_1 = document.getElementById("form3Example4cg").value;
     var password_2 = document.getElementById("form3Example4cdg").value;
-    var agree = document.getElementById("form2Example3cg").value;
+    var agree = document.getElementById("form2Example3cg");
     if(id.charAt(0) <= 97 || id.charAt(0) >= 122 || id.length != 10){
         alert("身分證輸入錯誤");
         return false;
---
+++ php/SMTP.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!isset($_POST["submit"]))
+  {
+  ?>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  From: <input type="text" name="from"><br>
+  Subject: <input type="text" name="subject"><br>
+  Message: <textarea rows="10" cols="40" name="message"></textarea><br>
+  <input type="submit" name="submit" value="Click To send mail">
+  </form>
+  <?php
+  }
+
+else
+
+  {
+
+  if (isset($_POST["from"]))
+    {
+    $from = $_POST["from"]; // sender
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $message = wordwrap($message, 70);
+
+    mail("james0120160379@gmail.com",$subject,$message,"From: $from\n");
+    echo "Thank you for sending an email";
+    }
+  }
+?>
