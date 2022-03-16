304bb90621bd93db5255adf261052bb356f71d50
D33064 <james0120160379@gmail.com>
2022-03-16 15:56:11 +0800

    .

--- JS/register.js
+++ JS/register.js
@@ -2,10 +2,16 @@ function test(){
 
 var num_1 = document.getElementById("form3Example4cg").value;
 var num_2 = document.getElementById("form3Example4cdg").value;
+var check = document.getElementById("form2Example3cg").value;
 if(num_1 != num_2){
-    alert("密碼輸錯");
+    alert("密碼輸入錯誤");
     return false;
 }
+else if(!check.checked){
+    alert("確認同意條款");
+    return false;
+}
+return true;
 
 
 }
