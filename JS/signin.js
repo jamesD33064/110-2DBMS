b67dd7f94ea7e48c2a9f96e52b9ced59ce548eb3
jamesD33064 <james0120160379@gmail.com>
2022-05-09 18:18:16 +0800

    .

---
+++ JS/signin.js
@@ -0,0 +1,56 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+a=0;
+document.getElementById("hamburger").addEventListener('click', function(){
+  if(a==0){
+    document.getElementById("main-continer").style.display = "none";
+    a=1;
+  }
+  else{
+    document.getElementById("main-continer").style.display = "block";
+    a=0;
+  }
+  
+});
