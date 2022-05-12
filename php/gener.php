bdff2ae6f3219ef17353df89c2511c462403a8dd
jamesD33064 <james0120160379@gmail.com>
2022-05-12 16:51:27 +0800

    .

---
+++ php/gener.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $place_id=$_SESSION["id"];
+    $urlToEncode="https://mspredator.com/110-2DBMS/110-2DBMS/php/qrcode.php?place_id=".$place_id; 
+    generateQRfromGoogle($urlToEncode); 
+
+    function generateQRfromGoogle($chl,$widhtHeight ='150',$EC_level='L',$margin='0') 
+    { 
+        $chl = urlencode($chl); 
+        echo '<img src="http://chart.apis.google.com/chart?chs='.$widhtHeight.'x'.$widhtHeight.'&cht=qr&chld='.$EC_level.'|'.$margin.'&chl='.$chl.'" alt="QR code" widhtHeight="'.$widhtHeight.'" widhtHeight="'.$widhtHeight.'"/>'; 
+    } 
+?>
+
