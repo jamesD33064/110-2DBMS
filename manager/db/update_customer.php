ecf4f51f3d464ab53691489f03723ec2f497a891
D33064 <james0120160379@gmail.com>
2022-06-12 20:28:37 +0800

    .

--- manager/db/update_customer.php
+++ manager/db/update_customer.php
@@ -5,7 +5,7 @@ $customer_id=$_POST["customer_id"];
 $change_customer=$_POST["change_customer"];
 
 $sql="UPDATE customer SET is_epidemic = '".$change_customer."' WHERE '".$customer_id."'= customer_id;";
-
+$footprint = "SELECT DISTINCT(place_id) FROM footprint_data WHERE customer_id = '".$customer_id."' AND time > DATE_ADD('".$time."', INTERVAL -3 DAY);";
 if(mysqli_query($conn,$sql)){
     
 }
